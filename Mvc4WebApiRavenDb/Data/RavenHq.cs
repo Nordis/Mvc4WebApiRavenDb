3d1c732a032cdcdb1a2f8ee227e5bb8c619d5673
Daniel Nordmark <nordmark.daniel@gmail.com>
2012-03-10 16:31:21 +0100

    Enable RavenHQ

---
+++ Mvc4WebApiRavenDb/Data/RavenHq.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Raven.Abstractions.Data;
+using Raven.Client;
+using Raven.Client.Document;
+
+namespace Mvc4WebApiRavenDb.Data
+{
+    public class RavenHq : IRavenDataStore
+    {
+        private static IDocumentStore instance;
+
+        public RavenHq()
+        {
+            InitDocumentStore();
+        }
+
+        #region Implementation of IRavenDataStore
+
+        public void InitDocumentStore()
+        {
+            var parser = ConnectionStringParser<RavenConnectionStringOptions>.FromConnectionStringName("RavenDB");
+            parser.Parse();
+
+            instance = new DocumentStore
+            {
+                ApiKey = parser.ConnectionStringOptions.ApiKey,
+                Url = parser.ConnectionStringOptions.Url,
+            };
+
+            instance.Initialize();
+        }
+
+        public IDocumentStore Instance
+        {
+            get { return instance; }
+        }
+
+        #endregion
+    }
+}
